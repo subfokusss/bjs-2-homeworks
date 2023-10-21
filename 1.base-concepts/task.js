988c6259c4f465bb4596957d3908aa895704c1d3
Subfokus <nkoleva@mail.ru>
2023-10-21 19:27:48 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+"use strict";
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+function solveEquation(a, b, c) {
+  var discriminant = b ** 2 - 4 * a * c;
+  var result = [];
+
+  if (discriminant < 0) {
+    return result;
+  } else if (discriminant === 0) {
+    var root = -b / (2 * a);
+    result.push(root);
+  } else {
+    var sqrtDiscriminant = Math.sqrt(discriminant);
+    var root1 = (-b + sqrtDiscriminant) / (2 * a);
+    var root2 = (-b - sqrtDiscriminant) / (2 * a);
+
+    result.push(root1, root2);
+  }
+
+  return result;
 }
