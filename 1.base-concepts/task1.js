8b7992312945e0f8078397ee849dcb706b94b2c8
Subfokus <nkoleva@mail.ru>
2023-10-25 14:15:52 +0500

    Второе задание переделано

--- 1.base-concepts/task1.js
+++ 1.base-concepts/task1.js
@@ -1,19 +1,6 @@
 "use strict";
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  if (
-    typeof percent !== "number" ||
-    typeof contribution !== "number" ||
-    typeof amount !== "number" ||
-    typeof countMonths !== "number"
-  ) {
-    return "Ошибка ввода. Все аргументы должны быть числами.";
-  }
-
-  if (percent < 0 || contribution < 0 || amount < 0 || countMonths < 0) {
-    return "Ошибка ввода. Все аргументы должны быть положительными числами.";
-  }
-
   const percentPerMonth = percent / 100 / 12;
   const loanAmount = amount - contribution;
 
