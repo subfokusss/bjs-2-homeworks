6e47e29ebbc03274ed2fd150f32ef96fe4646f21
Subfokus <nkoleva@mail.ru>
2023-11-14 00:24:37 +0500

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+      this.marks.push(...marksToAdd);
+    }
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+      return 0;
+    }
   
-}
-
-Student.prototype.exclude = function (reason) {
+    const sum = this.marks.reduce((total, mark) => total + mark, 0);
+    return sum / this.marks.length;
+  }
   
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
